6d192971b4209d6b5b68e09ffa46221133686c35
pulga <akindeleodedoyin@gmail.com>
2021-03-16 15:16:37 +0100

    added test to ensure that article gets created

--- pages/tests.py
+++ pages/tests.py
@@ -1,3 +1,14 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from django.contrib.auth.models import User
+from .models import Article
 
-# Create your tests here.
+
+class ArticleTests(APITestCase):
+    def test_create_article(self):
+        user = User.objects.create_user('lauren', 'secret')
+        self.client.force_authenticate(user=user)
+        data = {'paragraph': 'test article', 'author': 1}
+        response = self.client.post('/api/v1/', data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
